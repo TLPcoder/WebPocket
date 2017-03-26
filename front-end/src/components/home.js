ae6c856e37dc4febbbb11b85d1bb22661b8d1cb3
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-03-25 19:09:20 -0700

    I am happy with this but I am upset about the iframe limits

--- front-end/src/components/home.js
+++ front-end/src/components/home.js
@@ -7,21 +7,47 @@ import * as BookmarkAction from '../actions/bookmark-actions';
 import * as CategoryAction from '../actions/category-actions';
 import axios from 'axios';
 import Bookmarks from './bookmarks';
+import Navbar from './navbar';
 
 class Home extends Component{
     constructor(props){
         super(props);
     }
+    setStyle(style){
+        this.setState({style:style});
+    }
     render(){
+        var style;
+        if(this.props.store.selectedBookmark.name){
+            style ={
+                textAlign: 'left',
+                textDecoration: 'none',
+                color: 'white',
+                padding: '8px 15px',
+                position: 'relative',
+                left:'230px',
+                top:'30px',
+                fontSize: '25px',
+                backgroundColor: '#CB3837',
+                border: 'solid 1px #CB3837',
+                webkitTransitionDuration: '.5s', /* Safari */
+                transitionDuration: '.5s'
+            };
+        }
         return(
-            <div className = "home-container">
-                <div className = "buttons-container">
-                    <Bookmarks/>
-                </div>
-                <div className = "iframe">
-                    <iframe src={this.props.store.selectedBookmark.url} frameBorder="0" width="900px" height='600px'>SORRY THIS PAGE DOES NOT ALLOW iFRAMES</iframe>
-                    <br/>
-                    <a href={this.props.store.selectedBookmark.url}>{this.props.store.selectedBookmark.name}</a>
+            <div>
+                <div className = 'background'></div>
+                <div className = "home-container">
+                    <Navbar/>
+                    <div className = "buttons-container">
+                        <Bookmarks/>
+                    </div>
+                    <div >
+                        <iframe className = "iframe" src={this.props.store.selectedBookmark.url} frameBorder="0" width="900px" height='600px'>SORRY THIS PAGE DOES NOT ALLOW iFRAMES</iframe>
+                        <br/>
+                        <a id = 'iframe-container' style={style} target='_blank'
+                        href={this.props.store.selectedBookmark.url}>{this.props.store.selectedBookmark.name}</a>
+                    </div>
                 </div>
             </div>
         )
