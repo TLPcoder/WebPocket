756cad8bf6bef573a55713a32b995e246fc51645
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-04-09 19:12:12 -0700

    refactor

---
+++ front-end/src/components/add-category.js
@@ -0,0 +1,95 @@
+"use strict";
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import axios from 'axios';
+import {bindActionCreators} from 'redux';
+import * as BookmarkAction from '../actions/bookmark-actions';
+import * as Category from '../actions/category-actions';
+
+class AddCategory extends Component{
+    constructor(){
+        super();
+        this.createCategory = this.createCategory.bind(this);
+        this.renderCategories = this.renderCategories.bind(this);
+        this.renderBookmarks = this.renderBookmarks.bind(this);
+    }
+    createCategory(){
+        var category_name = document.getElementById('add-category-text').value;
+        var user_id = sessionStorage.getItem('id');
+        console.log('safjasldkjf', category_name);
+        axios.post(`http://localhost:8000/create/category`,{
+            category_name: category_name,
+            user_id: user_id,
+        })
+        .then((response)=>{
+            this.props.actions.addCategory(false);
+            axios.get(`http://localhost:8000/API/category/${user_id}`)
+            .then((response) => {
+                this.props.actions.updateCategory(response.data);
+                this.props.actions.noCategories(false);
+                this.renderCategories();
+            });
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+
+    }
+    renderCategories(){
+        var key = 0;
+        return this.props.store.category.map((el) => {
+            key++;
+            return (<input key={key} type="button" className='button' onClick={()=>{
+                this.renderBookmarks(el.category_id)
+            }} value={el.category_name}/>)
+        });
+    }
+    renderBookmarks(categoryId = this.props.store.currentCategory){
+        var key = 0;
+        axios.get(`http://localhost:8000/API/bookmarks/category/${categoryId}`)
+        .then((response) => {
+            console.log("hello there data", response.data)
+            var bookmarks = response.data.map((el) => {
+                key++;
+                return (<input key={key} type="button" className='button' id ={el.category_id} onClick={()=>{
+                    var obj = {
+                        name:el.bookmark_name,
+                        url:el.url
+                    }
+                    this.props.actions.selectBookmark(obj)
+                }} value={el.bookmark_name}/>)
+            });
+            this.props.actions.bookmarks(bookmarks);
+            this.props.actions.currentCategory(categoryId);
+            this.props.actions.clearCategory(bookmarks);
+        });
+    }
+    render(){
+        return(
+            <div>
+                <div id='add-category'>
+                    <br/>
+                    <input type="text" placeholder="Add Category" id="add-category-text"/>
+                    <br/>
+                    <input type="button" className='create-category-button' value="Create Category" onClick={this.createCategory}/>
+                    <input type="button" value="Back" className='button-back' onClick={()=>{
+                        this.props.actions.addCategory(false);
+                        }
+                    }/>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(store){
+    return {store}
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        actions:bindActionCreators({...BookmarkAction,...Category},dispatch)
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(AddCategory)
