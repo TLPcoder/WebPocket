cc06aae1ab82a1c9d8bb3262d319f4c22ac1b64f
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-04-16 21:20:23 -0700

    add controller for create APIs bookmarks categories and users

--- backend/routers/API.js
+++ backend/routers/API.js
@@ -1,15 +1,8 @@
 "use strict";
 const express = require('express');
 const router = express.Router();
-var knex = require('../knex');
 const controller = require('../controllers/API-controllers');
 
-
-router.get('/', function(req, res) {
-    res.json({
-        message:"router is working"
-    });
-});
 router.get('/category/:id', controller.getCategories);
 
 router.get('/bookmarks/category/:id', controller.getBookmarks);
